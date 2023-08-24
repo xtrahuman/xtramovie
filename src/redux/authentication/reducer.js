9b0e963b2cd4a920a9d0f91b96e558a4ffde11a6
xtrahuman <okpara_tochukwu@yahoo.com>
2023-08-24 12:42:44 +0100

    update user loading feature

--- src/redux/authentication/reducer.js
+++ src/redux/authentication/reducer.js
@@ -1,54 +1,54 @@
 import {
-    GETLOGINSTART,
-    GETLOGINSUCCESS,
-    GETLOGINFAILURE,
-    LOGOUT,
-    AUTHENTICATED,
-  } from "./action";
-  
-  const INITIAL_STATE = {
-    user: {},
-    error: null,
-    userLoading: false,
-    loggedin: false,
-  };
-  
-  const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-      case GETLOGINSTART:
-        return {
-          ...state,
-          userLoading: true,
-          message: null,
-          loggedin: false,
-        };
-      case GETLOGINSUCCESS:
-      case AUTHENTICATED:
-        return {
-          ...state,
-          userLoading: false,
-          user: action.payload,
-          loggedin: true
-        };
-      case GETLOGINFAILURE:
-        return {
-          ...state,
-          userLoading: false,
-          error: action.payload,
-          message: null,
-          loggedin: false
-        };
-      case LOGOUT:
-        return {
-          ...state,
-          userLoading: false,
-          loggedin: false,
-          user: {},
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default userReducer;
-  
+  GETLOGINSTART,
+  GETLOGINSUCCESS,
+  GETLOGINFAILURE,
+  LOGOUT,
+  AUTHENTICATED,
+} from "./action";
+
+const INITIAL_STATE = {
+  user: {},
+  error: null,
+  userLoading: false,
+  loggedin: false,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GETLOGINSTART:
+      return {
+        ...state,
+        userLoading: true,
+        message: null,
+        loggedin: false,
+        error: null,
+      };
+    case GETLOGINSUCCESS:
+    case AUTHENTICATED:
+      return {
+        ...state,
+        userLoading: false,
+        user: action.payload,
+        loggedin: true,
+      };
+    case GETLOGINFAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.payload,
+        message: null,
+        loggedin: false,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        userLoading: false,
+        loggedin: false,
+        user: {},
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
